4157cd0716e94784a64e96e6ddb1e964290ac394
rahul <r.baphana@elsevier.com>
2017-06-25 03:14:16 +0530

    Refactored to optmize

--- src/my_math.py
+++ src/my_math.py
@@ -4,15 +4,15 @@ class Operations:
 
 
     def iterative_sum(self, numbers):
-        sum = 0
-        validated_numbers = [] if not numbers else numbers
+        sum = None if not numbers else numbers[-1]
+        validated_numbers = [] if not numbers else numbers[:-1]
         for e in validated_numbers:
             sum = self.add(sum, e)
         return sum
 
 
     def tail_rec_sum(self, numbers):
-        validated_numbers = [] if not numbers else numbers
+        validated_numbers = [] if not numbers else numbers[:-1]
         def reduce(index, numbers, acc):
             return acc if not numbers else reduce(index-1 , numbers[:-1], self.add(acc, numbers[index]))
-        return reduce(len(validated_numbers)-1, validated_numbers, 0)
+        return reduce(len(validated_numbers)-1, validated_numbers, None if not numbers else numbers[-1])

9985b465b02ecb41276c131c0aebdbecdfd602da
rahul <r.baphana@elsevier.com>
2017-06-25 02:51:07 +0530

    Refactored to minimal required code

--- src/my_math.py
+++ src/my_math.py
@@ -5,22 +5,14 @@ class Operations:
 
     def iterative_sum(self, numbers):
         sum = 0
-        validated_numbers = [] if self.is_valid(numbers) else numbers
+        validated_numbers = [] if not numbers else numbers
         for e in validated_numbers:
             sum = self.add(sum, e)
         return sum
 
 
     def tail_rec_sum(self, numbers):
-        validated_numbers = [] if self.is_valid(numbers) else numbers
-
+        validated_numbers = [] if not numbers else numbers
         def reduce(index, numbers, acc):
-            if self.is_valid(numbers):
-                return acc
-            else: 
-                return reduce(index-1 , numbers[:-1], self.add(acc, numbers[index]))
+            return acc if not numbers else reduce(index-1 , numbers[:-1], self.add(acc, numbers[index]))
         return reduce(len(validated_numbers)-1, validated_numbers, 0)
-
-
-    def is_valid(self, numbers):
-        return not numbers

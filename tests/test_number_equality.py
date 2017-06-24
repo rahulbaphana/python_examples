45d622b543f418cc425a8335782b84a97ec8c56c
rahul <r.baphana@elsevier.com>
2017-06-25 01:30:25 +0530

    Clean up of imports and commented code in test

--- tests/test_number_equality.py
+++ tests/test_number_equality.py
@@ -1,8 +1,7 @@
 import unittest
-from my_math import Operations
-import sys
 
-sys.dont_write_bytecode = True
+from my_math import Operations
+
 
 class CheckNumbers(unittest.TestCase):
     def should_test_int_float(self):
@@ -13,4 +12,3 @@ class CheckNumbers(unittest.TestCase):
 
     def should_test_add_two_parameters(self):
         self.assertEqual(Operations().add(1,2), 3)
-        # self.assertEqual(Operations().add("Hello","World!"), "HelloWorld!")       

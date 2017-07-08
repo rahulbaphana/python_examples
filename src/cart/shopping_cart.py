428f9361833d18d56f9f66fb3f9005f0fa8f77cc
Rahul Baphana <rahul.kbaphana@yahoo.com>
2017-07-08 14:23:03 +0530

    Using lambda with reduce

--- src/cart/shopping_cart.py
+++ src/cart/shopping_cart.py
@@ -1,11 +1,12 @@
 import functools
+from src.cart.product import Product
 
 class Cart:
     products = []
 
     def get_total_price(self):
-        add = lambda a,b : a + b
-        return functools.reduce(add, [product.price for product in self.products], 0)
+        add = lambda a,b : Product(f"adding ${a.name} ${b.name}", a.price + b.price)
+        return functools.reduce(add, self.products, Product("Total", 0)).price
 
     def add(self, product, quantity=1):
         self.products = [product] * quantity        

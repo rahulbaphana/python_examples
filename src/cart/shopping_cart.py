6638ce0205c0e56e1f0d31fbe2988687f58e90f3
Rahul Baphana <rahul.kbaphana@yahoo.com>
2017-07-08 14:12:57 +0530

    Refctored the Cart to use reduce(..,..,..)

--- src/cart/shopping_cart.py
+++ src/cart/shopping_cart.py
@@ -1,12 +1,11 @@
+import functools
+
 class Cart:
     products = []
 
     def get_total_price(self):
         add = lambda a,b : a + b
-        sum = 0
-        for product in self.products:
-            sum = sum + product.price 
-        return sum
+        return functools.reduce(add, [product.price for product in self.products], 0)
 
     def add(self, product, quantity=1):
         self.products = [product] * quantity        
